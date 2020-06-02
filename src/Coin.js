bc007b572a66fa2965088b52c3668d33568c7d52
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-01 22:07:20 -0700

    implement coin flip logics

--- src/Coin.js
+++ src/Coin.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import "./Coin.css";
 
 class Coin extends Component {
   render() {
-    return <img src=""></img>;
+    return (
+      <img
+        className="Coin"
+        src={`https://tinyurl.com/react-coin-${this.props.face}-jpg`}
+      ></img>
+    );
   }
 }
 
--- src/Panel.js
+++ src/Panel.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import Coin from "./Coin";
+import "./Panel.css";
 
 class Panel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numFlips: 0,
+      numHeads: 0,
+      curFace: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const rand = Math.floor(Math.random() * 2);
+    if (rand === 0) {
+      this.setState((prevState) => ({
+        numFlips: prevState.numFlips + 1,
+        numHeads: prevState.numHeads + 1,
+        curFace: "heads",
+      }));
+    } else {
+      this.setState((prevState) => ({
+        ...prevState,
+        numFlips: prevState.numFlips + 1,
+        curFace: "tail",
+      }));
+    }
+  }
+
   render() {
     return (
-      <div>
-        <H1></H1>
-        <Coin />
-        <Button></Button>
-        <p></p>
+      <div className="Panel">
+        <h1>Let's flip a coin</h1>
+        {this.state.curFace && <Coin face={this.state.curFace} />}
+        <button onClick={this.handleClick}>Flip Me</button>
+        <p>
+          Out of {this.state.numFlips} flips, there have been{" "}
+          {this.state.numHeads} heads and{" "}
+          {this.state.numFlips - this.state.numHeads} tails.
+        </p>
       </div>
     );
   }
