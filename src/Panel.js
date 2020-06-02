269fa3bb396600902ddbaaaff83853b064833bd2
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-01 21:43:16 -0700

    initial commit

---
+++ src/Panel.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Coin from "./Coin";
+
+class Panel extends Component {
+  render() {
+    return (
+      <div>
+        <H1></H1>
+        <Coin />
+        <Button></Button>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+export default Panel;
